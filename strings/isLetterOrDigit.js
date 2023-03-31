72851dfa849497ac46e4a669991e61789b4e3ba4
Yong Yu <yong.yu@gluwa.com>
2023-03-30 21:11:48 -0700

    - improved the code not to use regular expression

--- strings/isLetterOrDigit.js
+++ strings/isLetterOrDigit.js
@@ -1,6 +1,12 @@
-/* test if the given character is a-z, A-Z, 0-9
+/* test if the given character is a-z, A-Z, or 0-9
  * 
  */
 export default function isLetterOrDigit(c) {
-    return /[a-zA-Z\d]/.test(c); // no '_'; that would be [\w]
+    if (typeof c !== 'string') return false;
+
+    let c_codepoint = c.charCodeAt(0);
+    
+    return (c_codepoint >= 48 && c_codepoint <= 57) || 
+           (c_codepoint >= 65 && c_codepoint <= 90) || 
+           (c_codepoint >= 97 && c_codepoint <= 122);
 }
