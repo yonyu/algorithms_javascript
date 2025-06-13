67ec804f4e9d5e96af29e2073e61c3dc68bb23b5
Yong Yu <ypyu@hotmail.com>
2025-06-13 00:06:25 -0700

    Recursion review

--- src/recursion/factorial.js
+++ src/recursion/factorial.js
@@ -5,14 +5,51 @@
  * @param {number} number 
  * @returns {number}
  */
- export function factorial(number) {
+export function factorial(number) {
     if (number === 0) {
         return 1;
     }
     return number * factorial(number - 1);
 }
 
-console.log(factorial(5));
+/**
+ * Simulate recursion with iteration
+ * This explains the steps involved in recursive calculation of factorial.
+ * @param {*} number 
+ */
+export function factorial_2(input) {
+    if (input === 0) return 1;
+
+    let callStack = [];
+    callStack.push({"returnAddr": "start", "number": input}); // "Call" the "factorial() function"
+    let returnValue;
+
+    while (callStack.length > 0) {
+        // The body of the "factorial() function":
+        let number = callStack[callStack.length - 1]["number"];
+        let returnAddr = callStack[callStack.length -1]["returnAddr"];
+
+        if (returnAddr == "start") {
+            if (number === 1) {
+                // Base case
+                returnValue = 1;
+                callStack.pop(); // "Return" from "function call"
+                continue;
+            } else {
+                // Recursive case
+                callStack[callStack.length-1]["returnAddr"] = "after recursive call";
+                // "Call" the "factorial() function":
+                callStack.push({"returnAddr": "start", "number": number - 1})
+                continue;
+            }
+        } else if (returnAddr =="after recursive call") {
+            returnValue = number * returnValue;
+            callStack.pop(); // "Return" from "function call"
+            continue;
+        }
+    }
+    return returnValue;
+}
 
 /**
  * Iterative factorial
--- tests/recursion/factorial.test.js
+++ tests/recursion/factorial.test.js
@@ -1,13 +1,28 @@
-import { factorial, factorial_iterative } from '../../src/recursion/factorial';
+import { factorial, factorial_2, factorial_iterative } from '../../src/recursion/factorial';
 
 describe('factorial', () => {
     it('should return 1 for 0', () => {
         expect(factorial(0)).toBe(1);
+        expect(factorial(1)).toBe(1);
+        expect(factorial(3)).toBe(6);
+        expect(factorial(8)).toBe(40320);
+    });
+});
+
+describe('factorial_2', () => {
+    it('should return 1 for 0', () => {
+        expect(factorial_2(0)).toBe(1);
+        expect(factorial_2(1)).toBe(1);
+        expect(factorial_2(3)).toBe(6);
+        expect(factorial_2(8)).toBe(40320);
     });
 });
 
 describe('factorial_iterative', () => {
     it('should return 1 for 0', () => {
         expect(factorial_iterative(0)).toBe(1);
+        expect(factorial_iterative(1)).toBe(1);
+        expect(factorial_iterative(3)).toBe(6);
+        expect(factorial_iterative(8)).toBe(40320);
     });
 });
